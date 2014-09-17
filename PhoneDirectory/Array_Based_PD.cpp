06266992d5476d6df22b929b3e96b6e1574ace09
GrahamGroe <gegroe@gmail.com>
2014-09-17 15:12:27 -0700

    Final Project Graham Groe, everything implemented

--- PhoneDirectory/Array_Based_PD.cpp
+++ PhoneDirectory/Array_Based_PD.cpp
@@ -1,6 +1,6 @@
 /** Implementation of the Phone_Directory using an array
-	of entries
-	@author Koffman and Wolfgang
+of entries
+@author Koffman and Wolfgang
 */
 
 #include "Array_Based_PD.h"
@@ -12,25 +12,24 @@
 using namespace std;
 
 /** Construct an empty Phone_Directory
- */
+*/
 Phone_Directory::Phone_Directory() :
-size(0), capacity(100), the_directory(new Directory_Entry[100]),
-source_name("") {}
+size(0), capacity(100), the_directory(new Directory_Entry[100]), source_name("") {}
 
 /** Destroy the Phone_Directory
- */
+*/
 Phone_Directory::~Phone_Directory()
 {
 	delete[] the_directory;
 }
 
 /** Function to load the data file.
-	pre:  The directory storage has been created and it is empty.
-	If the file exists, it consists of the name-number pairs
-	on adjacent lines.
-	post: The data from the file is loaded into the directory.
-	@param source_name The name of the data file
-	*/
+pre:  The directory storage has been created and it is empty.
+If the file exists, it consists of the name-number pairs
+on adjacent lines.
+post: The data from the file is loaded into the directory.
+@param source_name The name of the data file
+*/
 void Phone_Directory::load_data(const string& source_name)
 {
 	// Remember the source name for use by save.
@@ -51,10 +50,10 @@ void Phone_Directory::load_data(const string& source_name)
 }
 
 /** Add an entry or change an existing entry.
-	@param name The name of the person being added or changed
-	@param number The new number to be assigned
-	@return The old number or, if a new entry, an empty string
-	*/
+@param name The name of the person being added or changed
+@param number The new number to be assigned
+@return The old number or, if a new entry, an empty string
+*/
 string Phone_Directory::add_or_change_entry(const string& name,
 	const string& number)
 {
@@ -72,9 +71,9 @@ string Phone_Directory::add_or_change_entry(const string& name,
 }
 
 /** Look up an entry.
-	@param name The name of the person
-	@return The number. If not in the directory, an empty string
-	*/
+@param name The name of the person
+@return The number. If not in the directory, an empty string
+*/
 string Phone_Directory::lookup_entry(const string& name) const
 {
 	int index = find(name);
@@ -87,11 +86,11 @@ string Phone_Directory::lookup_entry(const string& name) const
 }
 
 /** Function to save the directory.
-	pre:  The directory has been loaded with data
-	post: Contents of directory written back to the file in the
-	form of name-number pairs on adjacent lines.
-	modified is reset to false.
-	*/
+pre:  The directory has been loaded with data
+post: Contents of directory written back to the file in the
+form of name-number pairs on adjacent lines.
+modified is reset to false.
+*/
 void Phone_Directory::save()
 {
 	if (modified) {  // if not modified, do nothing
@@ -108,28 +107,39 @@ void Phone_Directory::save()
 }
 
 /** Remove an entry
-	post: The name is no longer in the directory
-	@param name The name to be removed
-	@return That person's name or an empty string
-	if not in the directory
-	*/
+post: The name is no longer in the directory
+@param name The name to be removed
+@return That person's name or an empty string
+if not in the directory
+*/
 string Phone_Directory::remove_entry(const string& name) // Exercise 1.7: please complete the remove_entry() method - Ed/Kent
+//Zeus
 {
+	int index = Phone_Directory::find(name); // Call function find to determine if name is already in the directory
+	string p_num = the_directory[index].get_number(); // set p_num = to the number of person being removed
+	if (index != -1) { //If name is already in directory
+		the_directory[index].set_number(" "); // Remove(overwrite) entry from the array and modify the size
+		for (int i = index; i < size - 1; i++) //search array from index (location in array being removed) to the end
+			the_directory[i] = the_directory[i + 1]; //shift everything from location being removed down up by 1 leaving the last position blank
+		size--; // reduce array size by 1 removing the blank space
+		return p_num;
+		//return the_directory[index].get_number(); //Return number
+	}
+	else
+		return ""; //Return empty string
 
 	// Hint: you can use the code below to shift names down in the directory to remove the selected entry specified by "index"
 	// for (int i = index; i < size - 1; i++)
-		// the_directory[i] = the_directory[i + 1];
-	// Testing commit - Ewalker
-	return "";
+	// the_directory[i] = the_directory[i + 1];
 }
 
 // Private method implementation
 
 /** Search the array for a given name.
-	@param name The name to be found
-	@return The index of the entry containing this name
-	or -1 if the name is not present
-	*/
+@param name The name to be found
+@return The index of the entry containing this name
+or -1 if the name is not present
+*/
 int Phone_Directory::find(const string& name) const
 {
 	for (int i = 0; i < size; i++) {
@@ -139,9 +149,9 @@ int Phone_Directory::find(const string& name) const
 	return -1;
 }
 /** Add a new name-number pair to the directory.
-	@param name The name to be added
-	@param number The number to be added
-	*/
+@param name The name to be added
+@param number The number to be added
+*/
 void Phone_Directory::add(const string& name,
 	const string& number)
 {
@@ -153,8 +163,8 @@ void Phone_Directory::add(const string& name,
 }
 
 /** Create a new array of directory entries with twice the capacity
-	of the current one.
-	*/
+of the current one.
+*/
 void Phone_Directory::reallocate()
 {
 	// Double the capacity.
--- PhoneDirectory/Array_Based_PD.h
+++ PhoneDirectory/Array_Based_PD.h
@@ -3,99 +3,106 @@
 #include <string>
 
 /** Specification file for the array-based phone directory.
- */
+*/
 class Phone_Directory
 {
 public:
 	/** Construct an empty phone directory.
-	 */
+	*/
 	Phone_Directory();
 
 	/** Destroy the phone directory when it is no longer needed.
-	 */
+	*/
 	~Phone_Directory();
 
 	/** Load the data file containing the directory, or
-		establish a connection with the data source.
-		@param source_name The name of the file (data source)
-		with the phone directory entries.
-		*/
+	establish a connection with the data source.
+	@param source_name The name of the file (data source)
+	with the phone directory entries.
+	*/
 	void load_data(const std::string& source_name);
 
 	/** Look up an entry.
-		@param name The name of the person to look up
-		@return The number associated with that person
-		or an empty string if name is not in
-		the directory
-		*/
+	@param name The name of the person to look up
+	@return The number associated with that person
+	or an empty string if name is not in
+	the directory
+	*/
 	std::string lookup_entry(const std::string& name) const;
 
 	/** Changes the number associated with the given name to
-		the new value, or adds a new entry with this name and
-		number.
-		@param name The name of the person
-		@param number The new number
-		@return The old value of number or an empty string
-		if this is a new entry
-		*/
+	the new value, or adds a new entry with this name and
+	number.
+	@param name The name of the person
+	@param number The new number
+	@return The old value of number or an empty string
+	if this is a new entry
+	*/
 	std::string add_or_change_entry(const std::string& name,
 		const std::string& number);
 
 	/** Removes the entry with the specified name from the directory.
-		@param name The name of the person
-		@return The person's name or an empty string if not in
-		the directory
-		*/
+	@param name The name of the person
+	@return The person's name or an empty string if not in
+	the directory
+	*/
 	std::string remove_entry(const std::string& name);
 
 	/** Writes the contents of the directory to the data file.
-	 */
+	*/
 	void save();
 
 private:
+	//Zeus
 	class Directory_Entry /* Exercise 1.6: Please complete the definition of the Directory_Entry class here. - Ed/Kent */
 	{
 	public:
-		Directory_Entry() {} // Default no-argument constructor 
-		   Directory_Entry(std::string the_name, std::string the_number) {
-			   // constructor not implemented yet
-		   }
-		   std::string get_name() const {
-			   return ""; // method not implemented yet
-		   }
-		   std::string get_number() const {
-			   return ""; // method not implemented yet
-		   }
-		   void set_number(const std::string& new_number) {
-			   // method not implemented yet
-		   }
+		Directory_Entry(){} //default constructor
+		Directory_Entry(std::string name, std::string number) //constructor accepting arguments containing string type variable for name and number
+		{
+			this->name = name; // set argument name to private name
+			this->number = number; // set argument number to number
+		};
+		const std::string get_name() // set const string function for obtaining the name stored in private
+		{
+			return name; //return name
+		};
+		const std::string get_number() //set const string function for obtaining the number stored in private
+		{
+			return number; //return number
+		};
+		void set_number(const std::string& new_number) //set function to modify the number stored in private
+		{
+			number = new_number; //asign new_number to number
+		};
 	private:
-
+		std::string name; //name of person in directory
+		std::string number; //number of person in directory
 	};
 
 	// Private Functions
 	/** Searches the array of directory entries for the name.
-		@param name The name to be found
-		@return The index of the entry containing the name, or size
-		if the name is not found
-		*/
+	@param name The name to be found
+	@return The index of the entry containing the name, or size
+	if the name is not found
+	*/
 	int find(const std::string& name) const;
 
 	/** Adds a new entry with the given name and number to the array
-		of directory entries.
-		@param name The name to be added
-		@param number The number to be added
-		*/
+	of directory entries.
+	@param name The name to be added
+	@param number The number to be added
+	*/
 	void add(const std::string& name, const std::string& number);
 
 	/** Removes the entry at the given index.
-		@param index The index of the entry to be removed
-		*/
+	@param index The index of the entry to be removed
+	*/
 	void remove_entry(int index);
 
 	/** Creates an new array of directory entries with twice the
-		capacity of the current one.
-		*/
+	capacity of the current one.
+	*/
 	void reallocate();
 
 	/** The number of entries in the directory. */
@@ -107,7 +114,7 @@ private:
 	/** The name of the data file that contains the directory data. */
 	std::string source_name;
 	/** Boolean flag to indicate whether the directory was
-		modified since it was either loaded or saved. */
+	modified since it was either loaded or saved. */
 	bool modified;
 };
 
